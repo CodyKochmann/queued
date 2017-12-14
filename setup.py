481e19df232501288a888d7efd3c4d9e32de7fb6
Cody Kochmann <kochmanncody@gmail.com>
2017-12-14 14:00:41 -0500

    first release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'queued',
   packages = ['queued'], # this must be the same as the name above
-  version = '2017.11.21',
+  version = '2017.12.14',
   install_requires = ["strict_functions"],
   description = 'simple function decorators that make python functions and generators queued and async for nonblocking operations',
   author = 'Cody Kochmann',
