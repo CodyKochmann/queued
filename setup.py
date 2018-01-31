b13fcb0305eae59d777a73fef9291c69f5bc3e18
Cody Kochmann <kochmanncody@gmail.com>
2018-01-31 16:16:48 -0500

    simplified the code base

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'queued',
   packages = ['queued'], # this must be the same as the name above
-  version = '2017.12.14',
+  version = '2018.1.31',
   install_requires = ["strict_functions"],
   description = 'simple function decorators that make python functions and generators queued and async for nonblocking operations',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/queued',
-  download_url = 'https://github.com/CodyKochmann/queued/tarball/2017.12.14',
+  download_url = 'https://github.com/CodyKochmann/queued/tarball/2018.1.31',
   keywords = ['queued', 'queue', 'async', 'nonblocking', 'blocking', 'thread', 'multiprocessing', 'parallel'],
   classifiers = [],
 )
