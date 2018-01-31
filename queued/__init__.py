00ab5b506499e8dce83520ec5d3f9aa96b8d5fe8
CodyKochmann <kochmanncody@gmail.com>
2018-01-31 15:27:54 +0000

    cleaned up the queued implementation with multiprocessing.pool

--- queued/__init__.py
+++ queued/__init__.py
@@ -1,228 +1,76 @@
-# -*- coding: utf-8 -*-
-# @Author: Cody Kochmann
-# @Date:   2017-12-12 14:01:56
-# @Last Modified 2017-12-14
-# @Last Modified time: 2017-12-14 13:59:12
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-from multiprocessing import Queue
-from functools import wraps
-from threading import Thread
-from time import sleep, time
+from functools import wraps, partial
+from multiprocessing.pool import ThreadPool
+from inspect import isgeneratorfunction
+from overload import overload
+from strict_functions import strict_globals
 
-try:
-    from strict_functions import noglobals, strict_globals
-except ImportError:
-    exit('failed importing strict_functions\n\nyou can fix this by running:\n    pip install strict_functions')
+def queued(fn, workers=1):
+    assert callable(fn), 'fn needs to be callable'
+    assert type(workers)==int
+    assert workers>0
+    #print('making {} into a queued function with {} workers'.format(fn, workers))
+    return wraps(fn)(
+        lambda *a, f=fn, p=ThreadPool(workers):(
+            p.starmap_async(f, [a])
+        )
+    )
+    
+@overload
+def queued(fn, workers=1):
+    ''' this adapts queued to accept generator functions as wrapped input '''
+    assert isgeneratorfunction(fn)
+    #print('converting coroutine fn into a function')
+    f=fn().send # convert the coroutine into a function
+    f(None) # start the coroutine
+    return wraps(fn)(queued(
+        (lambda *a, f=f:(f(*a) if len(a)==1 else f(a))),
+        workers
+    ))
 
-
-def log_quesizes(log_path):
-    ''' this writes the size of each of the queues to a log so you can profile queue pipelines '''
-    with open(log_path, 'a') as f:
-        prev_s = ''
-        try:
-            while 1:
-                sleep(0.2)
-                mah_queues = log_quesizes.queues
-                if len(mah_queues):
-                    s=(' '+' '.join('{}:{}'.format(q.name,int(q.qsize())) for q in mah_queues)+'\n')
-                    if s != prev_s:
-                        prev_s = s
-                        f.write(str(time())+s)
-        except NotImplementedError:
-            from logging import warning
-            warning('multiprocessing.Queue.qsize() is not supported by this system')
-
-log_quesizes.queues = []
-@strict_globals(log_quesizes=log_quesizes, Thread=Thread)
-def enable_queue_size_logging(log_path='queue_sizes.log'):
-    assert isinstance(log_path, str) and len(log_path), 'enable_queue_size_logging needs a non-empty string as its argument, not {}'.format(log_path)
-    Thread(target=log_quesizes, args=(log_path,)).start()
-
-
-@strict_globals(
-    noglobals=noglobals,
-    Queue=Queue,
-    Thread=Thread,
-    strict_globals=strict_globals,
-    wraps=wraps,
-    log_quesizes=log_quesizes
-)
-def queued(fn):
-    '''
-This decorator makes a function queued by putting it behind a queue
-and letting the function run in a separate thread. Use this for functions
-that you don't care about the output of like logging or things that waste
-a lot of the main threads time like sending data to another server
-
-Example usage below:
-```
-@queued
-def f2(a):
-    print('f2-called with',a)
-
-@queued
-def f1(a,b):
-    print('f1 called with',a,b)
-    print('f1 sending',a,'to f2')
-    f2(a=a)
+@overload
+def queued(workers=1):
+    ''' this adapts queued to take arguments before wrapping a function '''
+    assert type(workers)==int
+    assert workers>1
+    return partial(queued, workers=workers)
+    
+    
 
 
-for i in range(5):
-    f1(a=i,b=i+2)
-
-print('done sending things to f1')
-```
-    '''
-    fn_q = Queue()
-    fn_q.name = '{:}.{:}'.format(fn.__module__, fn.__name__)
-    fn_q.name = '{:}'.format(fn.__name__)
-    log_quesizes.queues.append(fn_q)
-
-    @noglobals
-    def feeder(fn, q):
-        q_g = q.get
-        while 1:
-            a,k = q_g()
-            fn(*a,**k)
-    #start_new_thread(feeder, (fn, fn_q))
-    Thread(target=feeder, args=(fn, fn_q), daemon=False).start()
-    @wraps(fn)
-    @strict_globals(fn_q=fn_q)
-    def wrapper(*a,**k):
-        fn_q.put((a,k))
-    return wrapper
-
-
-@strict_globals(
-    noglobals=noglobals,
-    Queue=Queue,
-    Thread=Thread,
-    wraps=wraps,
-    strict_globals=strict_globals,
-    log_quesizes=log_quesizes
-)
-def queued_generator(fn):
-    '''
-This decorator puts a generator function behind a queue and returns the
-attached queue's get function. this way, you send data to the generator
-like you would any other regular function but underneath the arguments
-are queued and ran by the generator once it can.
-
-This is useful for times when you have really complex programs and you
-want to split everything off into little async containers.
-
-Example usage below:
-```
-@queued_generator
-def step2():
-    # set up the inner environment here
-    from itertools import count
-    c = count()
-    # inputs sent to this function are processed in the loop below
-    while 1:
-        i = yield
-        print('step2-recieved', i, 'totaling', next(c), 'elements')
-
-
-@queued_generator
-def step1():
-    # inputs sent to this function are processed in the loop below
-    while 1:
-        i = yield
-        print('step1 recieved', i)
-        print('step1 sending', i, 'to step2')
-        step2(i)
-
-for i in range(10):
-    step1(i)
-
-print('done sending data to step1')
-```
-    '''
-    fn_q = Queue()
-    fn_q.name = '{:}.{:}'.format(fn.__module__, fn.__name__)
-    fn_q.name = '{:}'.format(fn.__name__)
-    log_quesizes.queues.append(fn_q)
-
-    gen = fn()
-    next(gen) # start the generator
-    @noglobals
-    def feeder(gen, q):
-        g_s = gen.send
-        q_g = q.get
-        while 1:
-            i = q_g()
-            if len(i) == 1:
-                g_s(i[0])
-            else:
-                g_s(i)
-    #start_new_thread(feeder,(gen, fn_q))
-    Thread(target=feeder, args=(gen, fn_q), daemon=False).start()
-    @wraps(fn)
-    @strict_globals(fn_q=fn_q)
-    def wrapper(*a):
-        fn_q.put(a)
-    return wrapper
-
-del Queue
-del wraps
-del Thread
-del sleep
-del time
-del log_quesizes
-
-if __name__ == '__main__':
-    #=============================================================
-    # queued tutorial below
-    #=============================================================
-
+def test():
+    ''' this tests/demos different uses of queued '''
+    from time import sleep
+    
     @queued
-    def f2(a):
-        print('f2-called with',a)
-
+    def my_adder(a, b):
+        ''' my_adder as a nonblocking function '''
+        sleep(1)
+        c=a+b
+        print(('adder',a,b,c))
+    
     @queued
-    def f1(a,b):
-        print('f1 called with',a,b)
-        print('f1 sending',a,'to f2')
-        f2(a=a)
-
-
-    for i in range(5):
-        f1(a=i,b=i+2)
-
-    print('done sending things to f1')
-
-    #=============================================================
-    # queued_generator tutorial below
-    #=============================================================
-
-    @queued_generator
-    def step2():
-        # set up the inner environment here
-        from itertools import count
-        c = count()
-        # inputs sent to this function are processed in the loop below
+    def adder_gen():
+        ''' my_adder as a nonblocking coroutine '''
         while 1:
-            i = yield
-            print('step2-recieved', i, 'totaling', next(c), 'elements')
+            a, b = yield
+            c=a+b
+            print(('gen',a,b,c))
 
-
-    @queued_generator
-    def step1():
-        # inputs sent to this function are processed in the loop below
-        while 1:
-            i = yield
-            print('step1 recieved', i)
-            print('step1 sending', i, 'to step2')
-            step2(i)
+    @queued(workers=3)
+    def three_adders(a, b):
+        ''' my_adder with multiple workers '''
+        sleep(1)
+        c=a+b
+        print(('three',a,b,c))
 
     for i in range(10):
-        step1(i)
-
-    print('done sending data to step1')
-
-
-
-
-
-
+        my_adder(i, i*2)
+        adder_gen(i, i*2)
+        three_adders(i, i*2)
+        
+if __name__ == '__main__':
+    # run the demo if this is the script being ran
+    test()
