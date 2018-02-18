15ca2b08a11c36edd6f99a237dc9bb1dfeaab5eb
Cody Kochmann <kochmanncody@gmail.com>
2018-02-18 13:31:29 -0500

    added support for multiprocessing and logging

--- queued/__init__.py
+++ queued/__init__.py
@@ -3,20 +3,46 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from functools import wraps, partial
 from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 from inspect import isgeneratorfunction
 from strict_functions import strict_globals, overload
+from greater_context import logged_exceptions
+import logging
+import atexit
+from time import sleep
 
-def queued(fn, workers=1):
+class default(object):
+    ''' this class is used as a shared namespace for queued's defaults '''
+    logger=logging.exception
+    workers=1
+    multiprocessing=False
+
+class storage(object):
+    queues = []
+
+@atexit.register
+def wait_for_queues_to_finish():
+    while all((not q._taskqueue.empty()) for q in storage.queues):
+        sleep(0.25)
+
+def call(fn, logger):
+    #logging.warning('call is being called with - {}'.format(locals()))
+    with logged_exceptions(logger):
+        return fn()
+
+def queued(fn, workers=1, logger=default.logger, multiprocessing=default.multiprocessing):
     assert callable(fn), 'fn needs to be callable'
     assert type(workers)==int
     assert workers>0
+    assert not isgeneratorfunction(fn)
     #print('making {} into a queued function with {} workers'.format(fn, workers))
+    pool = (Pool if multiprocessing else ThreadPool)(workers)
+    storage.queues.append(pool)
     return wraps(fn)(
-        lambda *a, f=fn, p=ThreadPool(workers):(
-            p.starmap_async(f, [a])
-        )
+        lambda *args, **kwargs: pool.starmap_async(call, [(partial(fn, *args, **kwargs), logger)])
     )
-    
+
+
 @overload
 def queued(fn, workers=1):
     ''' this adapts queued to accept generator functions as wrapped input '''
@@ -25,7 +51,7 @@ def queued(fn, workers=1):
     f=fn().send # convert the coroutine into a function
     f(None) # start the coroutine
     return wraps(fn)(queued(
-        (lambda *a, f=f:(f(*a) if len(a)==1 else f(a))),
+        lambda *a: f(*a) if len(a) == 1 else f(a),
         workers
     ))
 
@@ -35,33 +61,30 @@ def queued(workers=1):
     assert type(workers)==int
     assert workers>1
     return partial(queued, workers=workers)
-    
-    
-
 
 def test():
     ''' this tests/demos different uses of queued '''
-    from time import sleep
-    
     @queued
     def my_adder(a, b):
         ''' my_adder as a nonblocking function '''
-        sleep(1)
+        #sleep(1)
         c=a+b
         print(('adder',a,b,c))
-    
+
     @queued
     def adder_gen():
         ''' my_adder as a nonblocking coroutine '''
         while 1:
             a, b = yield
-            c=a+b
-            print(('gen',a,b,c))
+            with logged_exceptions():
+                c=a+b
+                print(('gen',a,b,c))
 
     @queued(workers=3)
     def three_adders(a, b):
         ''' my_adder with multiple workers '''
-        sleep(1)
+        print('three_adders',locals())
+        #sleep(1)
         c=a+b
         print(('three',a,b,c))
 
@@ -69,7 +92,7 @@ def test():
         my_adder(i, i*2)
         adder_gen(i, i*2)
         three_adders(i, i*2)
-        
+
 if __name__ == '__main__':
     # run the demo if this is the script being ran
     test()
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
+
+version = '2018.2.18'
+
 setup(
   name = 'queued',
   packages = ['queued'], # this must be the same as the name above
-  version = '2018.2.17.1',
-  install_requires = ["strict_functions"],
+  version = version,
+  install_requires = ["strict_functions","greater_context"],
   description = 'simple function decorators that make python functions and generators queued and async for nonblocking operations',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/queued',
-  download_url = 'https://github.com/CodyKochmann/queued/tarball/2018.2.17.1',
+  download_url = 'https://github.com/CodyKochmann/queued/tarball/{}'.format(version),
   keywords = ['queued', 'queue', 'async', 'nonblocking', 'blocking', 'thread', 'multiprocessing', 'parallel'],
   classifiers = [],
 )
